ec22cacb2aa6a4c485066189912d890fe658e808
lquan <1570599338@qq.com>
2022-01-22 19:34:29 +0800

    1、解决集群模式下重复初始化的问题 2、解决quart整合springboot的时候默认数据库为c3p0数据库连接池 3、解决分布式情况下quartz的同时执行调度任务 4、解决自定义quartz.properties的文件手动加载问题 5、待解决问题：为什么不能自动加载quartz.properties？？？

--- springboot-job/springboot-quartz/springboot-quartzweb/src/main/java/com/lquan/quartz/task/TestTask2.java
+++ springboot-job/springboot-quartz/springboot-quartzweb/src/main/java/com/lquan/quartz/task/TestTask2.java
@@ -11,7 +11,8 @@ import java.util.Date;
 /**
  * @Author:
  * @Date: 2018/9/11 17:15
- * @Description: 任务2
+ * @DisallowConcurrentExecution注解可以控制我们的相同类的定时任务同时只有一个执行。通常的场景是如果定时任务实际执行时间大于设定的定时任务执行间隔时间，就会导致同时多个任务并行。
+ *
  */
 public class TestTask2 implements Job {
     @Override
--- springboot-job/springboot-quartz/springboot-quartzweb/src/main/java/com/lquan/quartz/task/TestTask3.java
+++ springboot-job/springboot-quartz/springboot-quartzweb/src/main/java/com/lquan/quartz/task/TestTask3.java
@@ -18,7 +18,8 @@ import java.util.List;
 /**
  * @Author:
  * @Date:
- * @Description:
+ * @DisallowConcurrentExecution注解可以控制我们的相同类的定时任务同时只有一个执行。通常的场景是如果定时任务实际执行时间大于设定的定时任务执行间隔时间，就会导致同时多个任务并行。
+ *
  */
 public class TestTask3 implements Job {
 
