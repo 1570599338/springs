127c3dbe811da8c6e50355d3a0b3df5c5c406bc6
lquan <LQuan6915256>
2022-12-16 16:40:11 +0800

    springboot整合redis分别使用lettuce和jedis进行整合、并且在mybatis的工程中更合了一个统计访问url的小功能

---
+++ SpringBoot2/SpringBoot2-study2022/boot2-06-web-data-redis/src/test/java/com/lquan/boot/BootRedisApplicationTests.java
@@ -0,0 +1,36 @@
+package com.lquan.boot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Slf4j
+@SpringBootTest
+class BootRedisApplicationTests {
+
+    @Autowired(required = false)
+    RedisTemplate<String,String> redisTemplate;
+
+    @Autowired(required = false)
+    RedisConnectionFactory redisConnectionFactory;
+
+
+    @Test
+    void contextLoads() {
+
+       redisTemplate.opsForValue().set("lquan","20221216");
+        String lquan = redisTemplate.opsForValue().get("lquan");
+
+        log.info("redisGet:{}",lquan);
+
+        Class<? extends RedisConnectionFactory> aClass = redisConnectionFactory.getClass();
+
+        log.info("redis的Cli是：{}",aClass);
+
+
+    }
+
+}
