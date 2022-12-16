e5dbdf7813362ca6f1f2be8bae20107cec9114b0
lquan <LQuan6915256>
2022-12-16 21:02:37 +0800

    Junit5单元测试及相关的知识点

---
+++ SpringBoot2/SpringBoot2-study2022/boot2-07-test-junit5/src/main/java/com/lquan/boot/Boot207TestJunit5Application.java
@@ -0,0 +1,13 @@
+package com.lquan.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot207TestJunit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot207TestJunit5Application.class, args);
+    }
+
+}
