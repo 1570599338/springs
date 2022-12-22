be22b98d620cd88e55822036173e5f86fe4eef14
lquan <LQuan6915256>
2022-12-22 21:13:20 +0800

    前期的环境整合及服务支付模块

---
+++ springcould/cloud2022/cloud-provider-payment8001/src/main/java/com/lquan/bean/resp/CommonResult.java
@@ -0,0 +1,36 @@
+package com.lquan.bean.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @program: springcould
+ * @description:
+ * @author: lquan
+ * @create: 2022-12-22 19:48
+ **/
+@Data
+public class CommonResult<T> {
+    // 状态码
+    private Integer code;
+    // 消息
+    private String message;
+    // 数据传输
+    private T date;
+
+
+    public CommonResult() {
+    }
+
+    public CommonResult(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public CommonResult(Integer code, String message, T date) {
+        this.code = code;
+        this.message = message;
+        this.date = date;
+    }
+}
---
+++ springcould/cloud2022/cloud-provider-payment8001/src/main/java/com/lquan/domain/Payment.java
@@ -0,0 +1,40 @@
+package com.lquan.domain;
+
+import lombok.ToString;
+import java.io.Serializable;
+
+/**
+ * (Payment)实体类
+ *
+ * @author lquan
+ * @since 2022-12-22 19:44:23
+ */
+@ToString
+public class Payment implements Serializable {
+    private static final long serialVersionUID = -89200327775006604L;
+    /**
+     * id
+     */
+    private Long id;
+    
+    private String serial;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+}
+
