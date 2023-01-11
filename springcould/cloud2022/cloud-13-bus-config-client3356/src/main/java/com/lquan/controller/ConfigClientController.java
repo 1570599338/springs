5f221a0340ff54c32e4122a5beab0304408d9506
lquan <1570599338@qq.com>
2023-01-11 14:10:44 +0800

    spring config结合spring bus组件利用mq的订阅模式进行全局的动态配置文件的刷新

---
+++ springcould/cloud2022/cloud-13-bus-config-client3356/src/main/java/com/lquan/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.lquan.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: springcould
+ * @description:
+ * @author: lquan
+ * @create: 2023-01-08 16:04
+ **/
+@RestController
+@RefreshScope
+public class  ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
