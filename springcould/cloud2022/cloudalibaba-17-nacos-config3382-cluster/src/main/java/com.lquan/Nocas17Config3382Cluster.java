b903fed016c929d499eb3378e26877955d001e7b
lquan <1570599338@qq.com>
2023-01-13 17:47:07 +0800

    nacos的集群模式的在不使用ngix的时候实现的config

---
+++ springcould/cloud2022/cloudalibaba-17-nacos-config3382-cluster/src/main/java/com.lquan/Nocas17Config3382Cluster.java
@@ -0,0 +1,25 @@
+package com.lquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: springcould
+ * @description:
+ *              默认方式回去配置中心的配置信息
+ *
+ *
+ * @author: lquan
+ * @create: 2023-01-13 09:43
+ *
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Nocas17Config3382Cluster {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nocas17Config3382Cluster.class,args);
+    }
+
+}
